2853297ec190e4829c37d597aef814e9d7b0c10d
David Cifuentes <david.cifuentes@zalando.de>
2019-12-14 11:21:43 +0100

    Update deps and dynamic menu based on env

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -51,7 +51,7 @@ const Menu = () => {
         <ul>
           <li>
             <Link to="/" activeStyle={activeLinkStyle}>
-              Home (Localhost)
+              Home {process.env.NODE_ENV === `production` ? '(Prod)' : '(Dev)' }
             </Link>
           </li>
           <li>
